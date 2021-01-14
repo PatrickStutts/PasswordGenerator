188a20f47454a0c03fd128fe2e158065f9b764ce
PatrickStutts <patrickstutts@Patricks-MacBook-Air.local>
2021-01-14 13:22:18 -0800

    changed charCode to use vars for prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,30 @@
+// Create vars for characters
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "1234567890";
+var specialChar = "!@#$%^&*()?/;:~";
+
+ 
+// Ask for user input 
+    var askObject = {
+        askFunction: function(){
+        window.alert("To create your password, certain criteria must be met. Please answer the following questions:");
+        userPwLength = window.prompt("To start, how many characters long would you like your password to be?\nMinimum: 8 characters, and\nMaximum: 128 characters");
+        userUpper = window.confirm("We recommend you use a combination of upper and lowercase characters.\nWould you like to include capitalized letters?\nIf not, select 'cancel'.");
+        userLower = window.confirm("Would you like to include lowercase letters?\nIf not, select 'cancel'.");
+        userNumber = window.confirm("Would you like to include numeric characters in your password? If not, select 'cancel'.");
+        userSymbol = window.confirm("For added security, we suggest using special characters.\nWould you like to include special characters in your password?\nIf not, select 'cancel'.");
+    }
+//};
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+  var passLength = choosePassLength();
+  var characterTypes = chooseCharacterTypes();
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -10,39 +32,4 @@ function writePassword() {
 
 } 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); 
-
-//grabbing all lowercase letters between charcode columns 97-123
-var lowerCase = String.fromCharCode(Math.floor(Math.random() *26) + 97);
-
-//grabbing uppercase letters between charcode columns 65-91
-var upperCase = String.fromCharCode(Math.floor(Math.random() *26) +65); 
-
-//numbers 0-9 from charcode columns 48-58
-var number =  String.fromCharCode(Math.floor(Math.random() * 10) +48);
-
-//special symbols
-var symbol = "!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~/";
-
-
-var allowedCharacters = ""
-  if (characterTypes.hasLowerCase) {
-    allowedCharacters = allowedCharacters.concat(lowerCase)
-  }
-  if (characterTypes.hasUpperCase) {
-    allowedCharacters = allowedCharacters.concat(upperCase)
-  }
-  if (characterTypes.hasNumber) {
-    allowedCharacters = allowedCharacters.concat(numeric)
-  }
-  if (characterTypes.hasSymbol) {
-    allowedCharacters = allowedCharacters.concat(special)
-  }
-
-  var password = ""
-  for (var i = 0; i < passLength; i++) {
-    var randomIndex = Math.floor(Math.random() * allowedCharacters.length);
-    var randomCharacter = allowedCharacters[randomIndex];
-    password = password.concat(randomCharacter);
-   } 
-   
+generateBtn.addEventListener("click", writePassword);
